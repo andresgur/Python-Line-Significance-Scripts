083149b3dce6e77f856bef9f802e387f3e7b42c2
andresgur <andres.gurpide@gmail.com>
2018-04-26 08:57:41 +0200

    Added optional argument: number of scripts to be processed

--- pythonscripts/significance.py
+++ pythonscripts/significance.py
@@ -3,31 +3,62 @@
 # - the complex model: with line
 #the program substracts the C-stats that finds in both files and finally plots the C-stat probabily distribution of the data together with the C-stat provided with its significance
 # Author: Andres Gurpide Lasheras andres.gurpide@gmail.com
+#!/usr/bin/python
 import numpy as np
-import os
 import sys
 import matplotlib.pyplot as plt   
+
+
+#set the number of spectra to be read to infinite
+nspectra=-1;
+#check if a number of spectra to be processed has been provided by the user
+if len(sys.argv)>1:
+    
+     nspectra=int(sys.argv[1])
+
 
 delta_c= float(raw_input("Type delta C you want to test against: \n"))
 
 simple_model_file = raw_input("Type data file for the 'simple' model: \n")
 complex_model_file = raw_input("Type data file for the 'complex' model: \n")
 
-#read simple model
-with open(simple_model_file) as f:
-	lines = f.readlines()	
-	c_simple = [float(line.split(" ")[1]) for line in lines]
-	print "Number of lines in %s %i" %(simple_model_file,len(c_simple))
-#read simple model
-with open(complex_model_file) as f:
-	lines = f.readlines()	
-	c_complex = [float(line.split(" ")[1]) for line in lines]
-	print "Number of lines in %s %i" %(complex_model_file,len(c_complex))
+
+#case where the document has to be fully read
+
+if nspectra==-1:
+    #read simple model
+    with open(simple_model_file) as f:
+        lines = f.readlines()    
+        c_simple = [float(line.split(" ")[1]) for line in lines if not line.startswith("\n")]
+        print "Number of lines in %s %i" %(simple_model_file,len(c_simple))
+#read complex model
+        with open(complex_model_file) as f:
+            lines = f.readlines()    
+            c_complex = [float(line.split(" ")[1]) for line in lines if not line.startswith("\n")]
+            print "Number of lines in %s %i" %(complex_model_file,len(c_complex))
+            
+#case where the user has set some number of spectra to be read
+else:
+     print "Processing only first %i spectra" %nspectra
+        #read simple model
+     c_simple=[]
+     with open(simple_model_file) as f:
+             for i, line in enumerate(f):
+                 if not line.startswith("\n"):
+                        c_simple.append(float(line.split(" ")[1]))
+                        if i>nspectra:break
+  #read complex model
+     c_complex=[]
+     with open(complex_model_file) as f:
+            for i, line in enumerate(f):
+                if not line.startswith("\n"):
+                     c_complex.append(float(line.split(" ")[1]))
+                     if i>nspectra:break
 
 #exit program if the length of the monte carlo simulations is not the same
 if len(c_complex)!=len(c_simple):
-	print "Length of both files is not the same: %i vs %i, exiting program" %(len(c_simple),len(c_complex))
-	sys.exit()
+    print "Length of both files is not the same: %i vs %i, exiting program" %(len(c_simple),len(c_complex))
+    sys.exit()
 
 #compute differences and account for the significance of the line
 differences =[]
@@ -41,20 +72,20 @@ nsimulations=len(c_simple)
 for i in range(nsimulations):
 
 #get the difference in C between the simple and the complex model (complex should have lower C as it is a better fit usually)
-	difference = float(c_simple[i]-c_complex[i])
+    difference = float(c_simple[i]-c_complex[i])
 
-	if difference<0.0:
+    if difference<0.0:
 #warn the user that a negative delta C-stat value has been found
-		print "Warning! Spectrum %i has negative delta-c = %.3f" %(i,difference)
-		nnegative+=1
-		continue
-	differences.append(difference)
-	#print "Differences: %.3f for spectrum: %i" %(difference,i)
+        print "Warning! Spectrum %i has negative delta-c = %.3f" %(i,difference)
+        nnegative+=1
+        continue
+    differences.append(difference)
+    #print "Differences: %.3f for spectrum: %i" %(difference,i)
 
 #if a higher difference is found record it
-	if difference>delta_c:
-		nhigher+=1
-		print "Spectrum %i has higher delta-c = %.3f" %(i,difference)
+    if difference>delta_c:
+        nhigher+=1
+        print "Spectrum %i has higher delta-c = %.3f" %(i,difference)
 
 #substract the number of negative cstat simulations to the total to avoi accounting them
 totalsimulations = nsimulations-nnegative
@@ -78,10 +109,10 @@ bins = np.arange(0, 20, 0.1)
 #plot the result
 
 plt.hist(differences, bins=bins, normed=False)
-plt.title('Delta C-stat distribution')
+plt.title('%i simulations'%nsimulations)
 plt.xlabel('Delta C-stat bin')
 plt.ylabel('# spectra')
-plt.savefig("distribution_%.2f.pdf" %delta_c,bbox_inches='tight')
+plt.savefig("distribution_%.2f_%i_simulations.pdf" %(delta_c,totalsimulations),bbox_inches='tight')
 plt.show()
 
 
